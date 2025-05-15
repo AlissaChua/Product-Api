f9ce4d17e100e5fffe3c7b5e04d5691cf67b42bd
Alissa Chua <alissamaechua@gmail.com>
2025-05-15 14:06:26 +0800

    change the file directory

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-import { connectDB } from "./config/db.js";
+import { connectDB } from "./db.js";
 import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
