944c72d3d36dbcce529920294100703c898858b0
Alissa Chua <alissamaechua@gmail.com>
2025-05-15 13:31:11 +0800

    Initializes backend for product CRUD operations
    Sets up an Express server with routes, controllers, and a model for managing products.
    Integrates MongoDB for database persistence and includes an example environment configuration file.

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+import productRoutes from "./routes/product.route.js";
+
+dotenv.config();
+const app = express();
+
+app.use(express.json()); // allow accept json data in req.body
+
+app.use("/api/products",productRoutes);
+
+//  res.send("Server is ready"); knhzOhhO8tA2fG5R
+//console.log(process.env.MONGO_URI);
+app.listen(5000, () => {
+    connectDB();
+    console.log("Server started at http://localhost:5000");
+});
